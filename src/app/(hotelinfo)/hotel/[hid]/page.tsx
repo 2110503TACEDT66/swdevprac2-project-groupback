d6840ed57ffa8a421293681dda32a97013b2486f
Worapol Punthong <6633215921@student.chula.ac.th>
2024-03-26 21:38:25 +0700

    fix

--- src/app/(hotelinfo)/hotel/[hid]/page.tsx
+++ src/app/(hotelinfo)/hotel/[hid]/page.tsx
@@ -5,22 +5,24 @@ export default async function HotelDetailPage({params}:{params:{hid:string}}){
     
     const hotelDetail = await getHotel(params.hid)
     return(
-        <main className="text-center p-5" style={{ marginTop: '60px' }}>
+        <main  className="text-center p-5 mt-24" style={{ marginTop: '100px' }}>
             <h1 className="text-lg font-medium">{hotelDetail.data.name}</h1>
-            <p className="text-[0.5em] text-gray-500">{hotelDetail.data._id}</p>
-            <div className="flex flex-row my-5">
-                <div className="flex flex-col mx-5 my-1">
-                    <div className="text-md text-left my-1">Name: {hotelDetail.data.name}</div>
-                    <div className="text-md text-left my-1">Address: {hotelDetail.data.address}</div>
-                    <div className="text-md text-left my-1">District: {hotelDetail.data.district}</div>
-                    <div className="text-md text-left my-1">Province: {hotelDetail.data.province}</div>
-                    <div className="text-md text-left my-1">Tel: {hotelDetail.data.tel}</div>
-                
-                    <Link href={`/booking?id=${params.hid}&name=${hotelDetail.data.name}`}>
-                        <button className="block rounded-md bg-sky-600 hover:bg-indigo-600 px-3 py-2 text-whilte shadow-sm">
-                           Book a stay here
-                        </button>
-                    </Link>
+            <p className="text-xs text-gray-500">{hotelDetail.data._id}</p>
+            <div className="flex flex-col my-5 mx-auto max-w-md border border-gray-800 rounded-lg p-5">
+            <div className="flex flex-col my-1">
+                    <div className="text-md my-1">Name: {hotelDetail.data.name}</div>
+                    <div className="text-md my-1">Address: {hotelDetail.data.address}</div>
+                    <div className="text-md my-1">District: {hotelDetail.data.district}</div>
+                    <div className="text-md my-1">Province: {hotelDetail.data.province}</div>
+                    <div className="text-md my-1">Tel: {hotelDetail.data.tel}</div>
+                    <div className="text-md my-1">
+            </div>
+            <div className="text-md my-1 mt-auto">
+                <Link href={`/booking?id=${params.hid}&name=${hotelDetail.data.name}`}>
+                    <button className="block rounded-md bg-blue-600 hover:bg-blue-700 text-white px-3 py-2 mt-3 shadow-sm">                    Book a stay here
+                    </button>
+                </Link>
+            </div>
                 
                 </div>
             </div>
